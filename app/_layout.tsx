f47d630f52cd5d8a84b60934785a6a721f3d5bb4
jonaswoetmann <jonas.woetmann@gmail.com>
2025-03-26 21:27:26 +0100

    removed path title from all screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack
+          screenOptions={{ headerTitle: ' '}}
+      >
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
