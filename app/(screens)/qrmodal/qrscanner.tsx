33041e1a7a8187d761290268ee72af0be42b873c
jonaswoetmann <jonas.woetmann@gmail.com>
2025-03-26 21:19:44 +0100

    Added functional QR scanner

--- app/(screens)/qrmodal/qrscanner.tsx
+++ app/(screens)/qrmodal/qrscanner.tsx
@@ -1,13 +1,67 @@
-import { View, Text, Button } from 'react-native';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View, Linking } from 'react-native';
 import { useRouter } from 'expo-router';
 
 export default function QRScannerModal() {
-    const router = useRouter();
+  const [permission, requestPermission] = useCameraPermissions();
+  const [scanned, setScanned] = useState(false);
+  const router = useRouter();
 
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>QR Scanner</Text>
-            <Button title="close" onPress={() => router.back()} />
-        </View>
-    )
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="Grant Permission" />
+      </View>
+    );
+  }
+
+  const handleBarCodeScanned = ({ data }: { data: string }) => {
+    if (!scanned) {
+      setScanned(true);
+
+      Linking.openURL(data).catch(err=>console.error('Something went wrong!', err));
+      setTimeout(() => router.back(), 1000);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <CameraView
+        style={styles.camera}
+        facing="back"
+        barcodeScannerSettings={{
+          barcodeTypes: ['qr'],
+        }}
+        onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+      >
+      </CameraView>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+});
