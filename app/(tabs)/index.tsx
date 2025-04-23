301b266f0c3a47c3e8179465ae8288b97444e2ad
jonaswoetmann <jonas.woetmann@gmail.com>
2025-04-23 11:38:33 +0200

    corrected search overlay background color

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -186,7 +186,7 @@ const styles = StyleSheet.create({
         left: 0,
         right: 0,
         bottom: 0,
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        backgroundColor: '#fff',
         zIndex: 2,
         justifyContent: 'center',
         alignItems: 'center',
